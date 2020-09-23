04823bf55340c6246727b1e523184fa2d626a716
morganmccarley <morgan.mccarley@gmail.com>
2020-09-22 19:22:15 -0500

    adding the ability for th user to enter more than one value and break out with a sentinel value.

--- input_while.py
+++ input_while.py
@@ -10,17 +10,22 @@ if __name__ == '__main__':
 
     #declare a list variable
     my_list = []
+    #declare a sentinel value for user input
+    my_value = 0
 
     #prompt and get the user input
-    my_input = float(input("Enter a number between 1 and 100:"))
+    my_input = float(input("Enter a number between 1 and 100 (-1 to exit):"))
 
-    #while user input is not good (in range)
-    while my_input < 1 or my_input > 100:
-        #prompt user for good input
-        my_input = float(input("Enter a number between 1 and 100:"))
-
-    #store in list
-    my_list.append(my_input)
+    #while loop using sentinel value
+    while my_input != -1:
+        #while user input is not good (in range)
+        while my_input < 1 or my_input > 100:
+            #prompt user for good input
+            my_input = float(input("Enter a number between 1 and 100:"))
+        #store in list
+        my_list.append(my_input)
+        #prompt user for more input
+        my_input = float(input("Enter a number between 1 and 100 (-1 to exit):"))
 
     #print the list
     print(my_list)
